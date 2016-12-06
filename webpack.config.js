2a75ac9e1bdc27eedb2ba64a3ceb2b7f02911a86
Haythem Sahbani <EXTERN.SAHBANI_HAYTHEM@ALLIANZ.DE>
2016-12-06 10:25:38 +0100

    added components and index file

--- webpack.config.js
+++ webpack.config.js
@@ -161,7 +161,7 @@ module.exports = function makeWebpackConfig () {
     // Render index.html
     config.plugins.push(
       new HtmlWebpackPlugin({
-        template: './src/public/index.html',
+        template: './src/index.html',
         inject: 'body'
       }),
 
