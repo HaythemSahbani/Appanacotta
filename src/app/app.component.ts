58708e7ca16627cedab8c98b6ae7f781a661f71d
Haythem Sahbani <Haythem Sahbani>
2017-01-31 17:17:35 +0100

    added first map with tooltip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+ selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   lat: number = 48.1351;
   lng: number = 11.5820;
+  zoom: number = 16;
+  label: string = 'C';
+  infoText: string = `Hi, this is the content of the coffee info tooltip`;
 }
