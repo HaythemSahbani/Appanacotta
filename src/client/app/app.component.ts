7e14e396354c86642a69926023a5a6cd50507319
Haythem Sahbani <Haythem Sahbani>
2017-01-31 12:19:00 +0100

    modified project structure to fetch the angular style guide

---
+++ src/client/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/**
+ * This class represents the main application component.
+ */
+@Component({
+
+  selector: 'tm-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  constructor() { }
+}
